e94c06c8cfced9e2065fb29f17c7cdaee9f05cd4
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2022-03-08 15:21:00 +0800

    Add project files.

---
+++ deckofcards/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deckofcards
+{
+    public class App
+    {
+        //List
+
+        public App()
+        {
+        }
+
+        public string Choice()
+        {
+            //Add code in here...
+        }
+
+        public void CreateDeck()
+        {
+            //Add code in here...
+        }
+
+        public void ShuffleDeck()
+        {
+            //Add code in here...
+        }
+
+        public void DisplayDeck()
+        {
+            //Add code in here...
+        }
+
+        public void Deal(int num)
+        {
+            //Add code in here...
+        }
+    }
+}
---
+++ deckofcards/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace deckofcards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App();
+            //Add code in here...
+        }
+    }
+}
